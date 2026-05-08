20de717b943e967f55bdc86a602eb5164041ddc9
Adam Gutglick <adamgsal@gmail.com>
2026-05-08 19:06:22 +0100

    Clean up build, main and root
    Signed-off-by: Adam Gutglick <adamgsal@gmail.com>

--- justfile
+++ justfile
@@ -3,3 +3,10 @@ test:
 
 build:
     zig build
+
+docs:
+    zig build docs
+
+open-docs: docs
+    @echo "View docs at http://localhost:8000"
+    python3 -m http.server 8000 -d zig-out/docs/
--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,9 @@
 //! By convention, root.zig is the root source file when making a library.
-const std = @import("std");
-const Io = std.Io;
 
 pub const bit = @import("arrays/bit.zig");
 pub const primitive = @import("arrays/primitive.zig");
 pub const types = @import("types.zig");
 
 test {
-    std.testing.refAllDecls(@This());
+    @import("std").testing.refAllDecls(@This());
 }
